450cbd2bcc7467592becbdc26ea49ab9da3696cf
Tomasz Wisniewski <tomasz.wisniewski2@bsykb.com>
2014-04-28 12:05:23 +0100

    testcase skeleton added

---
+++ testcases/lib/NapiTest.pm
@@ -0,0 +1,29 @@
+#!/usr/bin/env perl
+
+package NapiTest;
+
+use strict;
+use warnings;
+use Exporter ();
+
+our @EXPORT = qw/
+	prepare_assets
+	prepare_shells
+	generate_tree
+/;
+
+
+sub prepare_assets {
+
+}
+
+sub prepare_shells {
+
+}
+
+sub generate_tree {
+	
+}
+
+
+1;
